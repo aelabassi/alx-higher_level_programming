22494a6960aa4b2826c67e5203e50cd4699ff246
aelabassi <abderrazaq.elabassi@uit.ac.ma>
2024-01-02 22:35:16 +0100

    last digit

--- 0x01-python-if_else_loops_functions/main.py
+++ 0x01-python-if_else_loops_functions/main.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
 uppercase = __import__("8-uppercase").uppercase
+print_last_digit = __import__("9-print_last_digit").print_last_digit
+
+print_last_digit(98)
+print_last_digit(0)
+r = print_last_digit(-1024)
+print(r)
 uppercase("best")
 uppercase("Best School 98 Battery street")
