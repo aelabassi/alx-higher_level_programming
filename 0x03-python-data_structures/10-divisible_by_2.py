8b366b1a7d413d4bcf2c0ec92f161e29283c6bea
aelabassi <abderrazaq.elabassi@uit.ac.ma>
2024-01-07 15:07:07 +0100

    Only by 2

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    if my_list:
-        is_multiple_of_2 = []
-        for i in my_list:
-            if i % 2 == 0:
-                is_multiple_of_2.append(True)
-            else:
-                is_multiple_of_2.append(False)
+    is_multiple_of_2 = []
+    for i in my_list:
+        if i % 2 == 0:
+            is_multiple_of_2.append(True)
+        else:
+            is_multiple_of_2.append(False)
     return is_multiple_of_2
