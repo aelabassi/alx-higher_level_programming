4f7927c21be1a993d3c354075a24bacd40486ec3
aelabassi <abderrazaq.elabassi@uit.ac.ma>
2024-01-07 14:49:44 +0100

    More returns

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,5 +1,8 @@
 #!/usr/python3
 def multiple_returns(sentence):
-    length = len(sentence)
-    first = sentence[0]
+    if sentence == "":
+        first = None
+    else:
+        first = sentence[0]
+        length = len(sentence)
     return (length, first)
