12958bcff03710dc89ff182d268cb04e6922634a
aelabassi <abderrazaq.elabassi@uit.ac.ma>
2024-01-10 15:53:21 +0100

    Best score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,12 +1,3 @@
 #!/usr/bin/python3
 def best_score(a_dictionary: dict):
-    if a_dictionary is None:
-        return None
-    else:
-        max_value = 0
-        max_key = ""
-        for k, v in a_dictionary.items():
-            if v > max_value:
-                max_value = v
-                max_key = k
-        return max_key
+    return max(a_dictionary, key=a_dictionary.get) if a_dictionary else None
