1ca84e98efdc1cb443b28caa899f7ddd6d23fbf6
aelabassi <abderrazaq.elabassi@uit.ac.ma>
2024-01-22 18:47:09 +0100

    0-Safe list print

--- 0-safe_print_list.py
+++ 0-safe_print_list.py
@@ -1,12 +1,11 @@
 #!/usr/bin/bash
 def safe_print_list(my_list=[], x=0):
-    try:
-        print(my_list[:x])
-        nb_print = 0
-        for i in my_list[:x]:
+    nb_print = 0
+    for i in range(x):
+        try:
+            print(my_list[i], end='')
             nb_print += 1
-    except IndexError:
-        pass
-    else:
-        return nb_print
-
+        except IndexError:
+            break
+    print("")
+    return nb_print
