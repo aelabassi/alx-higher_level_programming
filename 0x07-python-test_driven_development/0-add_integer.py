6e5c9e2a2b739656ef6faac2396242b43db7c951
aelabassi <abderrazaq.elabassi@uit.ac.ma>
2024-01-27 13:16:40 +0100

    Integers addition doctests

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+This function adds two non-Non integer types
+"""
+
+
+def add_integer(a, b=98):
+    """This function returns the sum of a and b
+        Args:
+            a (int): first int
+            b (int): second int
+        Return:
+            a + b (int): sum of a and b
+    """
+    if not isinstance(a, int) and not isinstance(a, float):
+        raise TypeError("a must be an integer")
+    elif not isinstance(b, int) and not isinstance(b, float):
+        raise TypeError("b must be n integer")
+    if type(a) == float:
+        a = int(a)
+    if type(b) == float:
+        b = int(b)
+    return a+b
