0ff69e52fd80c081e3e43cfdf4f09f14fcac11ac
aelabassi <abderrazaq.elabassi@uit.ac.ma>
2024-01-29 18:42:56 +0100

    3. String representation

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -79,11 +79,12 @@ class Rectangle:
     def __str__(self):
         """ instance print method (operator<<) """
         rect = []
-        if self.__height != 0 or self.__width != 0:
-            for i in range(self.__height):
-                for _ in range(self.__width):
-                    rect.append("#")
-                if i != self.__height - 1:
-                    rect.append("\n")
-            return "".join(rect)
-        return ""
+        if self.__height == 0 or self.__width == 0:
+            return ""
+
+        for i in range(self.__height):
+            for _ in range(self.__width):
+                rect.append("#")
+            if i != self.__height - 1:
+                rect.append("\n")
+        return "".join(rect)
