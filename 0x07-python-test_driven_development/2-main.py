8e7193c7d849b2c549f0f15faf4fa02f166926a3
aelabassi <abderrazaq.elabassi@uit.ac.ma>
2024-01-31 20:50:05 +0100

    fix second test

--- 0x07-python-test_driven_development/2-main.py
+++ 0x07-python-test_driven_development/2-main.py
@@ -2,8 +2,9 @@
 matrix_divided = __import__('2-matrix_divided').matrix_divided
 
 matrix = [
-    [1, 2, 3],
-    [4, 5, 6]
+    [2, 4, 6],
+    [8, 10, 12]
 ]
-print(matrix_divided(matrix, 3))
+matrix_2 = [[-1, 3.0, 5.6], [0, 4.7, -3.7]]
+print(matrix_divided(matrix_2, 2.2))
 print(matrix)
--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -18,7 +18,7 @@ def matrix_divided(matrix, div):
             not all((isinstance(el, int) or isinstance(el, float))
                     for el in [i for row in matrix for i in row])):
         raise TypeError("matrix must be a matrix (list of lists)"
-                        "of integers/floats")
+                        " of integers/floats")
     if not all(len(row) == len(matrix[0]) for row in matrix):
         raise TypeError("Each row of the matrix must have the same size")
     if not isinstance(div, int) and not isinstance(div, float):
