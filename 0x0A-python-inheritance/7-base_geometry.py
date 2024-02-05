96aa856c7ffdac9d4bdc2b4c25d482a5ce9a23a9
aelabassi <abderrazaq.elabassi@uit.ac.ma>
2024-02-05 17:49:38 +0100

    6. Imp

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+Improved BaseGeometry class
+"""
+
+
+class BaseGeometry:
+    """BaseGeometry class"""
+
+    def __init__(self):
+        pass
+
+    def area(self):
+        """area of the some geometry"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """validate value variable
+            Args:
+                name: (string)
+                value: (int)
+            Raises:
+                TypeError: value must be an int
+                ValueError: value must be > 0
+        """
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
