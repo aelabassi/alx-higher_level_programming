6c3d3bc0fd2591b10bcaf7a0357d95c4ef92d547
aelabassi <abderrazaq.elabassi@uit.ac.ma>
2024-02-06 18:35:21 +0100

    load, add, save

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -2,6 +2,7 @@
 """
 Adds argument to a python list and save them in json file
 """
+import json
 import sys
 
 if __name__ == "__main__":
@@ -10,6 +11,9 @@ if __name__ == "__main__":
     load_from_json_file = \
         __import__('6-load_from_json_file').load_from_json_file
 
-    items = []
+    try:
+        items = load_from_json_file("add_item.json")
+    except json.JSONDecodeError:
+        items = []
     items.extend(sys.argv[1:])
     save_to_json_file(items, "add_item.json")
