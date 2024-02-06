9a8bc4cbf1697421e9f441d1b5155af3839448cf
aelabassi <abderrazaq.elabassi@uit.ac.ma>
2024-02-06 17:45:07 +0100

    10. Student to disk and reload

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Student module
+"""
+
+
+class Student:
+    """Student class"""
+
+    def __init__(self, first_name, last_name, age):
+        """init a student instance"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """description of a class"""
+        if (type(attrs) == list and
+                all(type(el) == str for el in attrs)):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """sets attribute to reloaded json"""
+        for k, v in json.items():
+            setattr(self, k, v)
