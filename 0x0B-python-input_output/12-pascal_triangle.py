c5432271acf805a425791e0b0d436fb1e028a630
aelabassi <abderrazaq.elabassi@uit.ac.ma>
2024-02-06 17:49:32 +0100

    12. pascal traingle

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Defines a Pascal's Triangle function."""
+
+
+def pascal_triangle(n):
+    """Represent Pascal's Triangle of size n.
+    Returns a list of lists of integers representing the triangle.
+    """
+    if n <= 0:
+        return []
+
+    triangles = [[1]]
+    while len(triangles) != n:
+        tri = triangles[-1]
+        ones = [1]
+        for i in range(len(tri) - 1):
+            ones.append(tri[i] + tri[i + 1])
+        ones.append(1)
+        triangles.append(ones)
+    return triangles
