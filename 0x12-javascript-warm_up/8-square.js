6f4bead7de6fca42bb28178e7d75235688f428cf
aelabassi <abderrazaq.elabassi@uit.ac.ma>
2024-06-18 17:04:14 +0100

    print a square

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const size = process.argv[2];
+if (isNaN(size) || size === undefined) {
+  console.log('Missing size');
+} else {
+    const number = Number(size);
+  for (let i = 0; i < number; i++) {
+    console.log('X'.repeat(number));
+  }
+}
