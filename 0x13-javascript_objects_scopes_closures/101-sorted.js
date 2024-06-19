3d8eb96366afc2a244e680936d600af34126348a
aelabassi <abderrazaq.elabassi@uit.ac.ma>
2024-06-19 16:48:48 +0100

    sorted dict

---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+const dict = require('./101-data').dict;
+const listKV = Object.entries(dict);
+const values = Object.values(dict);
+const uniqueValues = [...new Set(values)];
+const sortedDict = {};
+for (const value of uniqueValues) {
+  sortedDict[value] = listKV.filter((kv) => kv[1] === value).map((kv) => kv[0]);
+}
+console.log(sortedDict);
