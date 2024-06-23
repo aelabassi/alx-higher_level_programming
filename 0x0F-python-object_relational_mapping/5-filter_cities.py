4f64cd48f82317aee6a64d48ef91ff444966d1f2
aelabassi <abderrazaq.elabassi@uit.ac.ma>
2024-06-23 23:41:33 +0100

    list cities of a state by name

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -6,10 +6,10 @@ from sys import argv
 import MySQLdb
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3], port=3306)
+    db = MySQLdb.connect(host="localhost", user=argv[1], passwd=argv[2], db=argv[3], port=3306)
     cursor = db.cursor()
     cursor.execute("SELECT cities.name FROM cities\
-        JOIN states ON cities.state_id = states.id \
+        INNER JOIN states ON cities.state_id = states.id \
         WHERE states.name = %s\
         ORDER BY cities.id", (argv[4],))
     rows = cursor.fetchall()
