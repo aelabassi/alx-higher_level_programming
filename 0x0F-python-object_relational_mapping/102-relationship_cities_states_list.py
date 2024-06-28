961eda81635d1d69b5fc19ceedacc08119b6c22d
Abdoelabassi <abderrezaq@outlook.com>
2024-06-28 22:20:40 +0100

    list all city objs correspond to state objs

---
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+list all cities from the database hbtn_0e_101_usa
+corresponding to the states
+"""
+from relationship_state import Base, State
+from relationship_city import City
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sys import argv
+
+if __name__ == "__main__":
+    username = argv[1]
+    password = argv[2]
+    database = argv[3]
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(username, password, database),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    for state in session.query(State).order_by(State.id):
+        for city in state.cities:
+            print("{}: {} -> {}".format(city.id, city.name, state.name))
+    session.close()
