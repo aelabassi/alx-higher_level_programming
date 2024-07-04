2bf69128fe13175e0b0b00ae49bf00fdfc4a9a4e
aelabassi <abderrazaq.elabassi@uit.ac.ma>
2024-07-04 18:30:59 +0100

    post request that sends a two variables

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # post request to the passed URL, and display the body of the response, and sends a variable email, and subject
-curl -s -X POST -d "email:test@gmail.com&subject:I will always be here for PLD" "$1"
+curl -s -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
