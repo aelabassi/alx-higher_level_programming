2c76e26ebd28107f1bb14b9ba64fbe7d4c3a9e06
aelabassi <abderrazaq.elabassi@uit.ac.ma>
2024-07-04 17:56:45 +0100

    get content length when curling a website

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/usr/bin/bash
 # script that takes in URL, sends request to URL, and displays size of body of response
 
 curl -sI "$1" | grep "Content-Length" | cut -d " " -f2
