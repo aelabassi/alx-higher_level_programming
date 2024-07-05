fba09a303eca54bec9b0e958710b75039b11ae03
aelabassi <abderrazaq.elabassi@uit.ac.ma>
2024-07-05 22:03:49 +0100

    10 recent commits

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""" script that list the 10 recent commits of a repository """
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = ('https://api.github.com/repos/{}/{}/commits'
+           .format(sys.argv[2], sys.argv[1]))
+    response = requests.get(url)
+    commits = response.json()
+    for commit in commits[:10]:
+        print("{}: {}".format(commit.get('sha'),
+                              commit.get('commit').get('author').get('name')))
