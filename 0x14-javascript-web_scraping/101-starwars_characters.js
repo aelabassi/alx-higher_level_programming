044c334f70f01bc333d7dde8b6470ac4a8fe80be
aelabassi <abderrazaq.elabassi@uit.ac.ma>
2024-07-30 11:52:17 +0100

    starwars characters

---
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -0,0 +1,35 @@
+#!/usr/bin/node
+const request = require('request');
+
+const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
+
+request(url, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    const characters = JSON.parse(body).characters;
+    characters.forEach((character) => {
+      request(character, function (error, response, body) {
+        if (error) {
+          console.log(error);
+        } else {
+          printCharacters(characters, 0);
+        }
+      });
+    });
+  }
+});
+
+const printCharacters = (characters, index) => {
+  if (index === characters.length) {
+    return;
+  }
+  request(characters[index], (error, response, body) => {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log(JSON.parse(body).name);
+      printCharacters(characters, index + 1);
+    }
+  });
+};
