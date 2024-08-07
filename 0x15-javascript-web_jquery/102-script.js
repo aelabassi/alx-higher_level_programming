ae8a855e20454a257bc846ed8c7ddf3c1310ee19
aelabassi <abderrazaq.elabassi@uit.ac.ma>
2024-08-07 23:22:53 +0100

    translate to many langs

---
+++ 0x15-javascript-web_jquery/102-script.js
@@ -0,0 +1,15 @@
+
+$(document).ready(() => {
+    $(() => {
+        $("INPUT#btn_translate").click(() => {
+            const lang = $("INPUT#language_code").val();
+            $.ajax({
+                url: `https://hellosalut.stefanbohacek.dev/?lang=${lang}`,
+                type: "GET",
+                success: (data) => {
+                    $("#hello").text(data.hello);
+                }
+            });
+        });
+    });
+});
